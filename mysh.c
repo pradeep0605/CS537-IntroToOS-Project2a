5d6fc5587a5fa644e889d611e5124df3996526ae
Pradeep Kashyap Ramaswamy <pradeep.kashyap@wisc.edu>
2016-10-08 22:34:02 -0500

    Basic String Processing

--- mysh.c
+++ mysh.c
@@ -47,35 +47,43 @@ void trim_line(char *s) {
   s[++index] = '\0';
 }
 
+/*
 void remove_redundant_spaces(char *s) {
   int index = 0;
   int i = 0;
   int spacecopied = false;
+  char *temp = malloc(strlen(s) + 1);
   
   trim_line(s);
 
   while (s[i] != '\0') {
-    if (s[i] == '\t')
+    if (s[i] == '\t') {
       s[i] = ' ';
+    }
     i++;
   }
 
   for (i = 0; i < strlen(s); ++i) {
-    if (s[i] == ' ' && !spacecopied) {
-      s[index] = s[i];
+    if ((s[i] == ' ' || s[i] == '\t') && !spacecopied) {
+      temp[index] = s[i];
       index++;
       spacecopied = true;
-    } else if (s[i] != ' ') {
-      s[index] = s[i];
+    }  
+    if (s[i] != ' ' || s[i] != '\t') {
+      temp[index] = s[i];
       index++;
       spacecopied = false;
     }
   }
-  s[index] = '\0';
-}
 
+  temp[index] = '\0';
+  strcpy(s, temp);
+  free(temp);
+}
+*/
 void prune_line(char *s) {
-  remove_redundant_spaces(s);
+  trim_line(s);
+  //remove_redundant_spaces(s);
 }
 int process_inbuild_command(char *s) {
   if (strcmp(s, "j") == 0) {
@@ -89,20 +97,28 @@ int process_inbuild_command(char *s) {
   return false;;
 }
 
+void process_line(char *s) {
+}
+
 int main(int argc, char *argv[])
 {
   char input[2000] = {0};
   if (argc == 2) {
     /* Work on batch mode */
     goto success;
-  } 
+  } else {
+    mysh_printf("Usage: mysh [batchFile]\n");
+  }
 
   while (1) {
     mysh_printf("%s", shell);
     read_line(input);
-    mysh_printf("%s\n", input);
-    prune_line(input);
-    mysh_printf("%s\n", input);
+    if (strlen(intput) > 512) {
+      mysh_printf("Command more than 512 characters\n");
+      continue;
+    }
+    process_line(input);
+    
     /* if in built command then process and return true */
     if (process_inbuild_command(input)) {
       continue;
